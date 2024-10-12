1d18b58ddeb18b86f59d880987399d6af51a0fbb
halchil <halzcreate1215@gmail.com>
2024-10-12 15:41:57 +0900

    commit

---
+++ docker_cleanup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Title: Docker Cleanup Script
+# Description: This script removes all running and stopped Docker containers, networks, and volumes.
+
+# Remove all containers (running and stopped)
+docker rm -f $(docker ps -aq)
+
+# Remove all networks
+docker network prune -f
+
+# Remove all volumes
+docker volume prune -f
+
+echo "Docker cleanup completed: All containers (running and stopped), networks, and volumes have been removed."
